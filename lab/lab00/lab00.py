64e7f3d1dd5d6675f39e5e89c05b6698bc143357
Alex Elman <pkill@linux.com>
2017-11-14 08:49:01 -0600

    Start on week 2

---
+++ lab/lab00/lab00.py
@@ -0,0 +1,11 @@
+def twenty_sixteen():
+    """Come up with the most creative expression that evaluates to 2016,
+    using only numbers and the +, *, and - operators.
+
+    >>> twenty_sixteen()
+    2016
+    """
+    return 3000 - 1000 + 16
+
+year = twenty_sixteen()
+print (year)
